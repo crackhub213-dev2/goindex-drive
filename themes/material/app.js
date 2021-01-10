b00d9da34ccfd49f4e17add2d8851d6fefde0005
crackhub213-dev2 <77217368+crackhub213-dev2@users.noreply.github.com>
2021-01-10 03:40:10 +0100

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -65,9 +65,14 @@ function nav(path) {
 		}
 	}
 	html += `<div class="mdui-toolbar-spacer"></div>
-    <a href="https://crackhub.site" target="_blank" class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-tooltip="{content: 'Website'}">
+    <a href="https://crackhub.site" target="_blank" class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-tooltip="{content: 'crackhub213'}">
       <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 36 36" enable-background="new 0 0 36 36" xml:space="preserve" class="mdui-icon" style="width: 24px;height:24px;">
-        <path fill-rule="evenodd" clip-rule="evenodd" fill="#ffffff" d="M240.362,335.53l49.396,13.746c0,0-17.942,18.174,3.96,34.251   c0.581-0.157,6.138-11.719-2.899-12.389l-0.289-1.566l17.4-8.414c0,0,12.116,6.292,6.059,19.106c0,0,21.365-12.58-4.139-29.359   l51.007-24.833c0,0,74.164,100.003-47.316,140.609c0,0,21.861-55.283-3.308-54.946c0,0-21.525-2.104,0.959,39.845   c0,0-89.936,6.376-83.56-76.177c0,0,3.02-24.498,10.739-36.579l1.847-2.958L240.362,335.53L240.362,335.53z"></path>
+        <path fill-rule="evenodd" clip-rule="evenodd" fill="#ffffff" d="M18,1.4C9,1.4,1.7,8.7,1.7,17.7c0,7.2,4.7,13.3,11.1,15.5
+	c0.8,0.1,1.1-0.4,1.1-0.8c0-0.4,0-1.4,0-2.8c-4.5,1-5.5-2.2-5.5-2.2c-0.7-1.9-1.8-2.4-1.8-2.4c-1.5-1,0.1-1,0.1-1
+	c1.6,0.1,2.5,1.7,2.5,1.7c1.5,2.5,3.8,1.8,4.7,1.4c0.1-1.1,0.6-1.8,1-2.2c-3.6-0.4-7.4-1.8-7.4-8.1c0-1.8,0.6-3.2,1.7-4.4
+	c-0.2-0.4-0.7-2.1,0.2-4.3c0,0,1.4-0.4,4.5,1.7c1.3-0.4,2.7-0.5,4.1-0.5c1.4,0,2.8,0.2,4.1,0.5c3.1-2.1,4.5-1.7,4.5-1.7
+	c0.9,2.2,0.3,3.9,0.2,4.3c1,1.1,1.7,2.6,1.7,4.4c0,6.3-3.8,7.6-7.4,8c0.6,0.5,1.1,1.5,1.1,3c0,2.2,0,3.9,0,4.5
+	c0,0.4,0.3,0.9,1.1,0.8c6.5-2.2,11.1-8.3,11.1-15.5C34.3,8.7,27,1.4,18,1.4z"></path>
       </svg>
     </a>`;
 	$('#nav').html(html);
